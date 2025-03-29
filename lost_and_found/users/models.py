5c253b647e170a531aad2c16a44c51dc3184f34b
ayoub essakhi <ayoubqe99@gmail.com>
2025-03-29 19:26:17 +0100

    Implement user authentication

--- lost_and_found/users/models.py
+++ lost_and_found/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    # Add additional fields like profile_picture if needed
+    pass
+
--- lost_and_found/users/views.py
+++ lost_and_found/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import generics
+from .serializers import CustomUserSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = CustomUserSerializer
+
+class LoginView(TokenObtainPairView):
+    pass
+
